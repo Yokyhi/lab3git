6c7c15078d80b5615236d84e9397da9eec415f23
Vereshchagin Max <qirl.cry@mail.ru>
2022-12-24 00:54:22 +0300

    Java класс для тестирования и библиотека Junit

---
+++ src/test/java/FunctionTest.java
@@ -0,0 +1,14 @@
+import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Assert;
+
+public class FunctionTest {
+
+    Function function;
+
+    @Before
+    public void createObject() {
+        function = new Function(10);
+    }
+}
